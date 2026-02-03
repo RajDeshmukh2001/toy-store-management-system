622fe9571ad043083ceebbca38b1cfacc626a369
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-03 09:00:42 +0530

    feat(api): add POST API endpoint to create Toy object

---
+++ src/app/api/toys/route.ts
@@ -0,0 +1,31 @@
+import { conn } from "@/lib/db";
+import Toy from "@/models/Toy";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    try {
+        const { name, price } = await request.json();
+        if (!name || name.length > 50) {
+            return NextResponse.json(
+                { error: "Name is required and must be less than 50 characters" },
+                { status: 400 }
+            );
+        }
+
+        if (price == null || price < 0 || typeof price !== "number") {
+            return NextResponse.json(
+                { error: "Price is required and must be a non-negative number" },
+                { status: 400 }
+            );
+        }
+
+        await conn();
+        await Toy.create({ name, price });
+        return NextResponse.json({ message: "Toy created successfully" }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
+}
